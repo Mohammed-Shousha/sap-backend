0571136ca81c797f2cf48c7f8bb60e02f61c13e3
Mohammed Shousha <mshousha29@gmail.com>
2023-04-05 23:14:18 +0200

    adding medicines resolvers

---
+++ queries/medicines.mjs
@@ -0,0 +1,22 @@
+import { ObjectId } from 'mongodb'
+
+export const allMedicines = async (medicines) => {
+   const data = await medicines.find().toArray()
+   return data
+}
+
+export const otcMedicines = async (medicines) => {
+   const data = await medicines.find({ otc: true }).toArray()
+   return data
+}
+
+export const medicineById = async (args, medicines) => {
+   const { id } = args
+
+   const data = await medicines.findOne({
+      _id: new ObjectId(id),
+      availableQuantity: { $gte: 1 }
+   })
+
+   return data
+}
