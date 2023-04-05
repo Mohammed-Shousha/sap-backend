1f83bd8355a26810305d584ce78529335aea53c8
Mohammed Shousha <mshousha29@gmail.com>
2023-04-05 23:17:41 +0200

    updating server

--- server.mjs
+++ server.mjs
@@ -1,17 +1,24 @@
 import express from 'express'
-import { ApolloServer } from '@apollo/server'
-import { expressMiddleware } from '@apollo/server/express4'
 import cors from 'cors'
+import stripe from 'stripe'
+import dotenv from 'dotenv'
 import bodyParser from 'body-parser'
 import { MongoClient } from 'mongodb'
-import dotenv from 'dotenv'
-import { registerUser, login, registerDoctor } from './mutations/users.mjs'
-import { addPrescription } from './mutations/prescriptions.mjs'
+import { ApolloServer } from '@apollo/server'
+import { expressMiddleware } from '@apollo/server/express4'
+import { registerUser, login, registerDoctor, deleteUsers } from './mutations/users.mjs'
+import { addPrescription, deletePrescriptions } from './mutations/prescriptions.mjs'
+import { allPrescriptions, prescriptionById, prescriptionsByUser } from './queries/prescriptions.mjs'
+import { allDoctors, allUsers, userById } from './queries/users.mjs'
+import { allMedicines, otcMedicines, medicineById } from './queries/medicines.mjs'
+import { addMedicines, updateMedicine, deleteMedicines } from './mutations/medicines.mjs'
 
 dotenv.config()
 
 const app = express()
-const { MONGO_URI, PORT } = process.env
+const { MONGO_URI, PORT, STRIPE_SECRET_KEY } = process.env
+
+const Stripe = stripe(STRIPE_SECRET_KEY)
 
 const client = await MongoClient.connect(MONGO_URI, { useUnifiedTopology: true })
 
@@ -21,6 +28,8 @@ const users = db.collection('users')
 const prescriptions = db.collection('prescriptions')
 
 const typeDefs = `#graphql
+
+   scalar Date
 
    type Medicine {
       _id: ID!
@@ -42,18 +51,19 @@ const typeDefs = `#graphql
       name: String!
       email: String!
       password: String!
-      doctor: Boolean!
-      prescriptions: [Prescription!]
+      isDoctor: Boolean!
    }
 
    type PrescriptionMedicine {
       medicineId: ID!
+      medicineName: String!
       quantity: Int!
       doctorInstructions: String!
    }
 
    input PrescriptionMedicineInput {
       medicineId: ID!
+      medicineName: String!
       quantity: Int!
       doctorInstructions: String!
    }
@@ -61,58 +71,63 @@ const typeDefs = `#graphql
    type Prescription {
       _id: ID!
       patientId: ID!
+      patientName: String!
       doctorId: ID!
+      doctorName: String!
       medicines: [PrescriptionMedicine!]
-      date: String!
-      purchased: Boolean!
+      date: Date!
+      isPaid: Boolean!
+      isRecived: Boolean!
    }
 
    type Query {
       medicines: [Medicine!]
+      otcMedicines: [Medicine!]
+      medicineById(id: ID!): Medicine!
       users: [User!]
+      doctors: [User!]
+      user(id: ID!): User!
       prescriptions: [Prescription!]
+      prescriptionById(id: ID!): Prescription!
+      prescriptionsByUser(userId: ID!): [Prescription!]
    }
 
    type Mutation {
-      resisterUser(name: String!, email: String!, password: String!): User!
+      registerUser(name: String!, email: String!, password: String!): User!
       registerDoctor(name: String!, email: String!, password: String!, licenseNumber: String!): User!
       login(email: String!, password: String!): User!
       addPrescription(patientId: ID!, doctorId: ID!, medicines: [PrescriptionMedicineInput!]!): Prescription!
-
       deleteUsers: Boolean!
       deletePrescriptions: Boolean!
+      addMedicines: Boolean!
+      updateMedicine(id: ID!, orderedQuantity: Int!): Medicine!
+      deleteMedicines: Boolean!
    }
    
    `
 
 const resolvers = {
    Query: {
-      medicines: async () => {
-         const data = await medicines.find().toArray()
-         return data
-      },
-      users: async () => {
-         const data = await users.find().toArray()
-         return data
-      },
-      prescriptions: async () => {
-         const data = await prescriptions.find().toArray()
-         return data
-      }
+      user: (_, args) => userById(args, users),
+      users: () => allUsers(users),
+      doctors: () => allDoctors(users),
+      prescriptions: () => allPrescriptions(prescriptions),
+      prescriptionById: (_, args) => prescriptionById(args, prescriptions),
+      prescriptionsByUser: (_, args) => prescriptionsByUser(args, prescriptions),
+      medicines: () => allMedicines(medicines),
+      otcMedicines: () => otcMedicines(medicines),
+      medicineById: (_, args) => medicineById(args, medicines),
    },
    Mutation: {
-      resisterUser: (_, args) => registerUser(args, users),
       login: (_, args) => login(args, users),
+      registerUser: (_, args) => registerUser(args, users),
       registerDoctor: (_, args) => registerDoctor(args, users),
+      deleteUsers: () => deleteUsers(users),
       addPrescription: (_, args) => addPrescription(args, prescriptions),
-      deleteUsers: async () => {
-         await users.deleteMany({})
-         return true
-      },
-      deletePrescriptions: async () => {
-         await prescriptions.deleteMany({})
-         return true
-      }
+      deletePrescriptions: () => deletePrescriptions(prescriptions),
+      addMedicines: () => addMedicines(medicines),
+      updateMedicine: (_, args) => updateMedicine(args, medicines),
+      deleteMedicines:() => deleteMedicines(medicines),
    },
 }
 
@@ -123,13 +138,7 @@ const server = new ApolloServer({
    resolvers,
    // cors: false,
 })
-
 await server.start()
-
-// app.use(cors({
-//    origin: 'http://localhost:3000',
-//    credentials: true
-// }))
 
 app.use(
    '/graphql',
@@ -137,6 +146,30 @@ app.use(
    bodyParser.json(),
    expressMiddleware(server)
 )
+
+app.use(express.json())
+
+// app.use(cors({
+//    origin: 'http://localhost:3000',
+//    credentials: true
+// }))
+
+
+app.post('/payment-sheet', async (req, res) => {
+   const { amount } = req.body
+
+   const paymentIntent = await Stripe.paymentIntents.create({
+      amount: amount * 100,
+      currency: 'usd',
+      automatic_payment_methods: {
+         enabled: true,
+      },
+   })
+
+   res.json({
+      paymentIntent: paymentIntent.client_secret,
+   })
+})
 
 
 app.listen({ port }, () =>
