e3e272b2f7265a13ae1bd27f154cbdea12372218
Mohammed Shousha <mshousha29@gmail.com>
2023-04-05 23:16:11 +0200

    adding prescriptions resolvers

--- mutations/prescriptions.mjs
+++ mutations/prescriptions.mjs
@@ -1,17 +1,35 @@
+import { ObjectId } from 'mongodb'
+import { joinPipeline } from '../utils/join.mjs'
+
 export const addPrescription = async (args, prescriptions) => {
    const { patientId, doctorId, medicines } = args
 
    const data = await prescriptions.insertOne({
-      patientId,
-      doctorId,
+      patientId: new ObjectId(patientId),
+      doctorId: new ObjectId(doctorId),
       medicines,
       date: new Date(),
-      purchased: false,
+      isPaid: false,
+      isRecived: false,
    })
 
-   const prescription = await prescriptions.findOne({
-      _id: data.insertedId,
-   })
+   const prescription = await prescriptions.aggregate([
+      {
+         $match: {
+            _id: data.insertedId,
+         }
+      },
+      ...joinPipeline
+   ]).toArray()
 
-   return prescription
+
+   return prescription[0]
+}
+
+//TODO: calclate prescription total price
+// using medicines price and quantity
+
+export const deletePrescriptions = async (prescriptions) => {
+   await prescriptions.deleteMany({})
+   return true
 }
---
+++ queries/prescriptions.mjs
@@ -0,0 +1,39 @@
+import { ObjectId } from 'mongodb'
+import { joinPipeline } from '../utils/join.mjs'
+
+export const allPrescriptions = async (prescriptions) => {
+   const data = await prescriptions.find().toArray()
+   return data
+}
+
+export const prescriptionById = async (args, prescriptions) => {
+   const { id } = args
+
+   const data = await prescriptions.aggregate([
+      {
+         $match: {
+            _id: new ObjectId(id)
+         }
+      },
+      ...joinPipeline
+   ]).toArray()
+
+   return data[0]
+}
+
+export const prescriptionsByUser = async (args, prescriptions) => {
+   const { userId } = args
+
+   const data = await prescriptions.aggregate([
+      {
+         $match: {
+            $or: [
+               { patientId: new ObjectId(userId) },
+               { doctorId: new ObjectId(userId) }
+            ]
+         }
+      },
+      ...joinPipeline
+   ]).toArray()
+   return data
+}
